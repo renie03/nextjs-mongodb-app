5a642bd18dc6feca1effcd279d251a743c38689c
Renie Adjani <renieadjani4@gmail.com>
2025-10-21 01:51:23 +0800

    aa3

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,7 +3,6 @@ const TestPage = () => {
     <div className="flex flex-col items-center gap-5 py-5">
       <img src="/forest.avif" alt="" className="h-[800px] w-[600px]" />
       <img src="/forest.avif" alt="" className="h-[800px] w-[600px]" />
-      <img src="/forest.avif" alt="" className="h-[800px] w-[600px]" />
     </div>
   );
 };
