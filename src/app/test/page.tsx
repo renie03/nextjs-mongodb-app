e25cc1f15efc29d868d16e9e74b374bb41777d8a
Renie Adjani <renieadjani4@gmail.com>
2025-10-21 01:47:36 +0800

    aa2

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,15 +1,9 @@
-import Image from "next/image";
-
 const TestPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Image
-        src="/forest.avif"
-        width={500}
-        height={500}
-        alt="forest"
-        className="h-[500px] w-[500px]"
-      />
+    <div className="flex flex-col items-center gap-5 py-5">
+      <img src="/forest.avif" alt="" className="h-[800px] w-[600px]" />
+      <img src="/forest.avif" alt="" className="h-[800px] w-[600px]" />
+      <img src="/forest.avif" alt="" className="h-[800px] w-[600px]" />
     </div>
   );
 };
