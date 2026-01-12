3ef836f19c05039726b701393e5e6156f5e2b8b0
Renie Adjani <renieadjani4@gmail.com>
2026-01-13 07:20:52 +0800

    new11

--- components/site/RegisterForm.tsx
+++ components/site/RegisterForm.tsx
@@ -162,7 +162,7 @@ const RegisterForm = () => {
       </div>
       <input type="hidden" value={avatar || ""} {...registerForm("image")} />
       <button
-        className="bg-blue-600 dark:bg-blue-700 hover:bg-blue-700 dark:hover:bg-blue-800 text-white rounded-md p-3 cursor-pointer disabled:cursor-not-allowed disabled:bg-blue-700 dark:disabled:bg-blue-800"
+        className="bg-blue-600 dark:bg-blue-700 hover:bg-blue-700 dark:hover:bg-blue-800 text-white rounded-md p-3 cursor-pointer disabled:cursor-not-allowed disabled:bg-blue-400 dark:disabled:bg-blue-500"
         disabled={isPending || isUploading}
       >
         {isPending ? <div className="spinner" /> : "Register"}
